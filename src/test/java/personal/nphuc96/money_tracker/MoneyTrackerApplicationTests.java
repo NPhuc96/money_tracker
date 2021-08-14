be68ab319600fc573e93a9030ad89aac3f1805e3
phuc <nguyenphuc.vna@gmail.com>
2021-08-14 09:19:31 +0700

    inital project

---
+++ src/test/java/personal/nphuc96/money_tracker/MoneyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.nphuc96.money_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
